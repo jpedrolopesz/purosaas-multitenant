c1765a2985d40b30db5b4586dcf8d18eaf89220b
jpedrolopesz <jplopeszamonelo@hotmail.com>
2022-09-08 17:30:57 -0300

    Initial commit

--- vendor/laravel/framework/src/Illuminate/Auth/Middleware/EnsureEmailIsVerified.php
+++ vendor/laravel/framework/src/Illuminate/Auth/Middleware/EnsureEmailIsVerified.php
@@ -24,7 +24,7 @@ class EnsureEmailIsVerified
             ! $request->user()->hasVerifiedEmail())) {
             return $request->expectsJson()
                     ? abort(403, 'Your email address is not verified.')
-                    : Redirect::guest(URL::route($redirectToRoute ?: 'verification.notice'));
+                    : Redirect::guest(URL::route($redirectToRoute ?: 'tenant.verification.notice'));
         }
 
         return $next($request);
