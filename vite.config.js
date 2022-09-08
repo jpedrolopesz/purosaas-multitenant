c6f192473c05fc04340f490e68371c4033261123
jpedrolopesz <jplopeszamonelo@hotmail.com>
2022-09-08 17:23:53 -0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/styles.css',
+                'resources/js/index.js',
             ],
             refresh: true,
         }),
