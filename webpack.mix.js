60b7c69ba7e52101d9e8815a39d9d6ac91a56550
jpedrolopesz <jplopeszamonelo@hotmail.com>
2022-09-09 18:24:32 -0300

    Initial commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('public/js/app.js', 'public/js')
-    .postCss('public/css/app.css', 'public/css', [
+mix.js('./public/js/app.js', './public/js')
+    .postCss('./public/css/app.css', './public/css', [
         require('tailwindcss'),
         require('postcss-import')
     ]).disableNotifications();
