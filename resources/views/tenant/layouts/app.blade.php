279179dad1f299410aadb226ca2fd0ee84784892
jpedrolopesz <jplopeszamonelo@hotmail.com>
2022-09-15 12:27:22 -0300

    Initial commit

--- resources/views/tenant/layouts/app.blade.php
+++ resources/views/tenant/layouts/app.blade.php
@@ -15,7 +15,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
 
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('./css/app.css') }}">
 
 
         <!-- Scripts -->
