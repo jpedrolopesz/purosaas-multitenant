73bdb405944aba3e8ce5272b51354b41ee8a2a69
jpedrolopesz <jplopeszamonelo@hotmail.com>
2022-09-19 18:09:09 -0300

    Demo

--- resources/views/central/layouts/admin.blade.php
+++ resources/views/central/layouts/admin.blade.php
@@ -38,5 +38,8 @@
             @yield('content')
     </main>
 </div>
+<!-- Start of HubSpot Embed Code -->
+<script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/22712815.js"></script>
+<!-- End of HubSpot Embed Code -->
 </body>
 </html>
