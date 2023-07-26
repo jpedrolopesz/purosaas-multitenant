36550449311a6738f22aab62b40315ae3855c377
jpedrolopesz <jplopeszamonelo@hotmail.com>
2023-07-26 18:03:36 -0300

    Commit

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,7 +17,9 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'purosaas.com',
+        '67.205.180.2'
+
+        //'purosaas.com',
         //'localhost'
     ],
 
