538786a3740813d79e6cceff7173535187cbd2bd
jpedrolopesz <jplopeszamonelo@hotmail.com>
2023-07-30 17:55:22 -0300

    Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        /**
         Admin::create([
             'name' => 'Admin Name',
             'company' => 'Company Name',
@@ -31,18 +32,15 @@ class DatabaseSeeder extends Seeder
             'remember_token' => Str::random(10),
         ]);
 
+         Plan::create(['name' => 'Gold', 'url'=> 'gold',  'teams_limit' => 2, 'slug' => 'monthly', 'price'=> 990, 'stripe_id' => 'price_1LSRHkGQW0U1Pfq']);
+         Plan::create(['name' => 'Gold', 'url'=> 'gold', 'teams_limit' => 2,'slug' => 'yearly', 'price'=> 9990, 'stripe_id' => 'price_1LTBIAGQW0U1Pf']);
 
-         Plan::create(['name' => 'Gold', 'url'=> 'gold',  'teams_limit' => 2, 'slug' => 'monthly', 'price'=> 990, 'stripe_id' => 'price_1LSRHkGQW0U1PfqxJO7EGsHx']);
-         Plan::create(['name' => 'Gold', 'url'=> 'gold', 'teams_limit' => 2,'slug' => 'yearly', 'price'=> 9990, 'stripe_id' => 'price_1LTBIAGQW0U1Pfqxdr82APbW']);
+         Plan::create(['name' => 'Diamond', 'url'=> 'diamond','teams_limit' => 5, 'slug' => 'monthly', 'price'=> 3990, 'stripe_id' => 'price_1LSmsIGQW0U1Pfqx']);
+         Plan::create(['name' => 'Diamond', 'url'=> 'diamond','teams_limit' => 5, 'slug' => 'yearly', 'price'=> 33990, 'stripe_id' => 'price_1Lb6p6GQW0U1Pfq']);
 
-
-         Plan::create(['name' => 'Diamond', 'url'=> 'diamond','teams_limit' => 5, 'slug' => 'monthly', 'price'=> 3990, 'stripe_id' => 'price_1LSmsIGQW0U1PfqxxsV7db73']);
-         Plan::create(['name' => 'Diamond', 'url'=> 'diamond','teams_limit' => 5, 'slug' => 'yearly', 'price'=> 33990, 'stripe_id' => 'price_1Lb6p6GQW0U1PfqxjKs6uVX2']);
-
-         Plan::create(['name' => 'Premium', 'url'=> 'premium', 'teams_limit' => 20,'slug' => 'monthly', 'price'=> 6990, 'stripe_id' => 'price_1LSRIVGQW0U1PfqxnFnKpQmh']);
-         Plan::create(['name' => 'Premium', 'url'=> 'premium', 'teams_limit' => 20,'slug' => 'yearly', 'price'=> 66990, 'stripe_id' => 'price_1Lb6kvGQW0U1PfqxQFJJcEBN']);
-
-
+         Plan::create(['name' => 'Premium', 'url'=> 'premium', 'teams_limit' => 20,'slug' => 'monthly', 'price'=> 6990, 'stripe_id' => 'price_1LSRIVGQW0U1Pf']);
+         Plan::create(['name' => 'Premium', 'url'=> 'premium', 'teams_limit' => 20,'slug' => 'yearly', 'price'=> 66990, 'stripe_id' => 'price_1Lb6kvGQW0U1']);
+         */
 
     }
 }
