64960a3da7c28e150822bd24e9fa62058a37078d
jpedrolopesz <67524586+jpedrolopesz@users.noreply.github.com>
2023-07-31 05:01:49 -0300

    Actualizar login.blade.php

--- resources/views/central/auth/login.blade.php
+++ resources/views/central/auth/login.blade.php
@@ -29,7 +29,7 @@
                 <div class="mt-6">
                     <label for="password" class="sr-only">Password</label>
                     <div class="mt-1 flex rounded-md shadow-sm">
-                        <input placeholder="demo1234" id="password" type="password" name="password" required autocomplete="new-password"  value="{{ old('password', 'demo1234') }}"
+                        <input placeholder="demo1234" id="password" type="password" name="password" required autocomplete="new-password"  value="{{ old('password', 'password') }}"
                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
                     </div>
                     @error('password')
